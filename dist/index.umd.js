e9a806ebbcd8cab7dae91e2eb14cff817ac80a76
Andreas Jacob <cordobo@web.de>
2017-09-24 17:26:43 +0200

    Version

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -1,6 +1,6 @@
 /**
  * angularx-qrcode - Angular4+ QRCode generator using qrcodejs
- * @version v1.0.0
+ * @version v0.0.1
  * @author Andreas Jacob <andreas@cordobo.de>
  * @link https://github.com/cordobo/angularx-qrcode#readme
  * @license MIT

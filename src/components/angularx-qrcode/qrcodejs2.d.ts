746e87ec82f96fdd6004d25a017c2cd6a0cb6dae
Andreas Jacob <cordobo@web.de>
2017-09-29 15:22:38 +0200

    d.ts

---
+++ src/components/angularx-qrcode/qrcodejs2.d.ts
@@ -0,0 +1,23 @@
+declare module 'qrcodejs2';
+
+declare class QRCode {
+    public static CorrectLevel: {
+        H: 2;
+        L: 1;
+        M: 0;
+        Q: 3;
+    };
+
+    constructor(el: HTMLElement, optionsOrText: string | {
+        colorDark?: string;
+        colorLight?: string;
+        correctLevel?: 0|1|2|3;
+        height?: number;
+        text?: string;
+        width?: number;
+    });
+
+    protected clear(): void;
+    protected makeCode(text: string): void;
+    protected makeImage(): void;
+}

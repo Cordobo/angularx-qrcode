93a00e5efe4ce951e55bf0745d1111d08e19a3be
Andreas Jacob <cordobo@web.de>
2018-03-15 21:24:17 +0100

    Indent, fixes

--- src/components/angularx-qrcode/angularx-qrcode.component.ts
+++ src/components/angularx-qrcode/angularx-qrcode.component.ts
@@ -1,71 +1,71 @@
 /// <reference path="./qrcodejs2.d.ts" />
 
 import {
-  Component,
-  Input,
-  ElementRef,
-  OnChanges,
-  OnInit,
-  SimpleChange,
-  ChangeDetectionStrategy
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Input,
+    OnChanges,
+    OnInit,
+    SimpleChange
 } from '@angular/core';
 
 import * as QRCode from 'qrcodejs2';
 
 @Component({
-  selector: 'qrcode',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  template: ''
+    selector: 'qrcode',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: ''
 })
 export class QRCodeComponent implements OnChanges, OnInit {
 
-  /** @internal */
-  @Input() public colordark: string = '#000000';
-  @Input() public colorlight: string = '#ffffff';
-  @Input() public level: string = 'M';
-  @Input() public hidetitle: boolean = false;
-  @Input() public qrdata: string = '';
-  @Input() public size: number = 256;
-  @Input() public usesvg: boolean = false;
+    /** @internal */
+    @Input() public colordark: string = '#000000';
+    @Input() public colorlight: string = '#ffffff';
+    @Input() public level: string = 'M';
+    @Input() public hidetitle: boolean = false;
+    @Input() public qrdata: string = '';
+    @Input() public size: number = 256;
+    @Input() public usesvg: boolean = false;
 
-  public qrcode: any;
+    public qrcode: any;
 
-  constructor(
-    public el: ElementRef
-  ) { }
+    constructor(
+        public el: ElementRef
+    ) { }
 
-  public ngOnInit() {
-    try {
-      if ( !this.isValidQrCodeText(this.qrdata) ) {
-        throw new Error('Empty QR Code data');
-      }
-      this.qrcode = new QRCode( this.el.nativeElement, {
-        colorDark: this.colordark,
-        colorLight: this.colorlight,
-        correctLevel: QRCode.CorrectLevel[this.level.toString()],
-        height: this.size,
-        text: this.qrdata,
-        useSVG: this.usesvg,
-        width: this.size,
-      });
-    } catch ( e ) {
-      console.error('Error generating QR Code: ' + e.message);
+    public ngOnInit() {
+        try {
+            if ( !this.isValidQrCodeText(this.qrdata) ) {
+                throw new Error('Empty QR Code data');
+            }
+            this.qrcode = new QRCode(this.el.nativeElement, {
+                colorDark: this.colordark,
+                colorLight: this.colorlight,
+                correctLevel: QRCode.CorrectLevel[this.level.toString()],
+                height: this.size,
+                text: this.qrdata,
+                useSVG: this.usesvg,
+                width: this.size,
+            });
+        } catch (e) {
+            console.error('Error generating QR Code: ' + e.message);
+        }
     }
-  }
 
-  public ngOnChanges( changes: {[propertyName: string]: SimpleChange} ) {
-    if ( !this.qrcode ) {
-      return;
+    public ngOnChanges = (changes: { [propertyName: string]: SimpleChange }) => {
+        if (!this.qrcode) {
+            return;
+        }
+        const qrData = changes['qrdata'];
+        if (qrData && this.isValidQrCodeText(qrData.currentValue)) {
+            this.qrcode.clear();
+            this.qrcode.makeCode(qrData.currentValue);
+        }
     }
-    const qrData = changes['qrdata'];
-    if ( qrData && this.isValidQrCodeText(qrData.currentValue) ) {
-      this.qrcode.clear();
-      this.qrcode.makeCode(qrData.currentValue);
-    }
-  }
 
-  protected isValidQrCodeText( data: string ) {
-    return !( typeof data === 'undefined' || data === '' );
-  }
+    protected isValidQrCodeText = (data: string): boolean => {
+        return !(typeof data === 'undefined');
+    }
 
 }

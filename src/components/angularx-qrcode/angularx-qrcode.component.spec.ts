2c58113fdfbd8aea0f664831dbdc82e2bf18006c
Andreas Jacob <cordobo@web.de>
2018-03-15 21:23:34 +0100

    Indent

--- src/components/angularx-qrcode/angularx-qrcode.component.spec.ts
+++ src/components/angularx-qrcode/angularx-qrcode.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { QRCodeComponent } from './angularx-qrcode.component';
 
 describe('QRCodeComponent', () => {
-  let component: QRCodeComponent;
-  let fixture: ComponentFixture<QRCodeComponent>;
+    let component: QRCodeComponent;
+    let fixture: ComponentFixture<QRCodeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ QRCodeComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [QRCodeComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(QRCodeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(QRCodeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

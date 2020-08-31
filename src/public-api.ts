1e6455740aa907b32f2a992e678b0ea48606bcf5
Andreas Jacob <cordobo@web.de>
2020-08-31 19:44:55 +0200

    fix: remove unused code

--- src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angularx-qrcode
- */
-
-export { QRCodeComponent } from './lib/angularx-qrcode.component';
-export { QRCodeModule } from './lib/angularx-qrcode.module';
-export * from './lib/types';

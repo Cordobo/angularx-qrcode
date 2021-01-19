e0a5bd68e8d5155eab89a9049b9edeee24e52ec8
Andreas Jacob <cordobo@web.de>
2021-01-19 21:44:16 +0100

    feat: add demo app as angular project #116

---
+++ projects/exampleApp/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'angularx-qrcode-sample-app';
+
+  public qrdata: string = null;
+  public elementType: 'img' | 'url' | 'canvas' | 'svg' = null;
+  public level: 'L' | 'M' | 'Q' | 'H';
+  public scale: number;
+  public width: number;
+
+  constructor() {
+    this.elementType = 'img';
+    this.level = 'M';
+    this.qrdata = 'Initial QR code data string';
+    this.scale = 1;
+    this.width = 256;
+  }
+
+  changeElementType(newValue: 'img' | 'url' | 'canvas' | 'svg'): void {
+    this.elementType = newValue;
+  }
+
+  changeLevel(newValue: 'L' | 'M' | 'Q' | 'H'): void {
+    this.level = newValue;
+  }
+
+  changeQrdata(newValue: string): void {
+    this.qrdata = newValue;
+  }
+
+  changeScale(newValue: number): void {
+    this.scale = newValue;
+  }
+
+  changeWidth(newValue: number): void {
+    this.width = newValue;
+  }
+}

aa2db9bbfa1b7ee5789fdfc48a3667355d7b36bb
Andreas Jacob <cordobo@web.de>
2022-01-07 18:55:07 +0100

    fix: add dist folder to ignore files #149

--- dist/angularx-qrcode/angularx-qrcode.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="angularx-qrcode" />
-export * from './public-api';
--- dist/angularx-qrcode/lib/types.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type QRCodeErrorCorrectionLevel = "low" | "medium" | "quartile" | "high" | "L" | "M" | "Q" | "H";
-export declare type QRCodeElementType = "url" | "img" | "canvas" | "svg";
-export declare type QRCodeVersion = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40;
-export declare type RGBAColor = `#${string}`;

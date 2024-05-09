001e31da144c9d0688bc676c14b67c3e40314fac
andreas <cordobo@web.de>
2024-05-09 03:20:43 +0200

    chore: static page deployment

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core"
+import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { ColorPickerModule } from "ngx-color-picker"
@@ -45,7 +45,6 @@ import { AppComponent } from "./app.component"
     MatTooltipModule,
     QRCodeModule,
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
